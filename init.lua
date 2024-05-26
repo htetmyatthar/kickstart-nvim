844aeb182d521872c11c84f2ee59756be0fc8a12
htetmyatthar <htetmyathm2002@gmail.com>
2024-05-26 14:45:04 +0630

    ADD: to use custom jdtls lsp config server

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 require("htetmyat")
-print("don't forget to use <leader>rn(rename) and <leader>Ge(iferrnil)")
+
+-- autocommands for jdtls
+local jdtls_lsp = vim.api.nvim_create_augroup("JdtlsGroup", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    require('jdtls_config.jdtls_setup').setup()
+  end,
+  group = jdtls_lsp,
+  pattern = "java",
+})

80efc55a4bd3af2e6bb57ee498c49d2407eaf98d
htetmyatthar <htetmyathm2002@gmail.com>
2024-05-26 14:47:43 +0630

    UPDATE: updated the gopher configuration with added cmt for caution with usage

--- lua/htetmyat/lazy/gopher.lua
+++ lua/htetmyat/lazy/gopher.lua
@@ -1,8 +1,19 @@
 return {
-	require('which-key').register {
-			['<leader>G'] = { name = '[A]dd automatically golang', _ = 'which_key_ignore' },
+	-- WARN: to use this plugin you must configure GOPATH, GOBIN to found the executables
+	{
+		"olexsmir/gopher.nvim",
+		requires = { -- dependencies
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		build = function()
+			vim.cmd [[silent! GoInstallDeps]]
+		end,
 	},
-	vim.keymap.set("n", "<leader>Gj", "<Cmd> GoTagAdd json <CR>", {desc = "add [G]o [J]son tag"}),
-	vim.keymap.set("n", "<leader>Gy", "<Cmd> GoTagAdd yaml <CR>", {desc = "add [G]o [Y]aml tag"}),
-	vim.keymap.set("n", "<leader>Ge", "<Cmd> GoIfErr <CR>", {desc = "add [G]o if [E]rr != nil"}),
+	require('which-key').register {
+		['<leader>G'] = { name = '[A]dd automatically golang', _ = 'which_key_ignore' },
+	},
+	vim.keymap.set("n", "<leader>Gj", "<Cmd> GoTagAdd json <CR>", { desc = "add [G]o [J]son tag" }),
+	vim.keymap.set("n", "<leader>Gy", "<Cmd> GoTagAdd yaml <CR>", { desc = "add [G]o [Y]aml tag" }),
+	vim.keymap.set("n", "<leader>Ge", "<Cmd> GoIfErr <CR>", { desc = "add [G]o if [E]rr != nil" }),
 }

c3ab61e95cbf921b55930f2db0cf70a605aab888
htetmyatthar <htetmyathm2002@gmail.com>
2024-05-28 06:23:07 +0630

    UPDATE: updated doc comment for file type configurations

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require("htetmyat")
 
--- autocommands for jdtls
+-- setting up jdtls for java file type.
 local jdtls_lsp = vim.api.nvim_create_augroup("JdtlsGroup", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   callback = function()

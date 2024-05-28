7d8635fea6e59c9157a46155de26bff697755934
htetmyatthar <htetmyathm2002@gmail.com>
2024-05-28 06:31:52 +0630

    UPDATE: fomatted with lsp

--- lua/htetmyat/lazy/telescope.lua
+++ lua/htetmyat/lazy/telescope.lua
@@ -13,7 +13,7 @@ return {
 		},
 	},
 	config = function()
-		require("telescope").setup{
+		require("telescope").setup {
 			defaults = {
 				mappings = {
 					i = {
@@ -22,34 +22,34 @@ return {
 					},
 				},
 			},
-			extensions ={
+			extensions = {
 				fzf = {},
 			},
 		}
 
 		pcall(require("telescope").load_extension, "fzf")
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles"})
-		vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "[S]earch [G]it files"})
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp"})
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps"})
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch [W]ord"})
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep"})
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics"})
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume"})
-		vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files"})
-		vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ]ind existing buffers"})
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "[S]earch [G]it files" })
+		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch [W]ord" })
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+		vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
+		vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ]ind existing buffers" })
 		vim.keymap.set("n", "<leader>/", function()
-		-- You can pass additional configuration to telescope to change theme, layout, etc.
-		builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown{
-			winblend = 10,
-			previewer = false,
-		})
-		end, { desc = "[/] Fuzzily search in current buffer"})
+			-- You can pass additional configuration to telescope to change theme, layout, etc.
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+				winblend = 10,
+				previewer = false,
+			})
+		end, { desc = "[/] Fuzzily search in current buffer" })
 
-      -- Shortcut for searching your neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+		-- Shortcut for searching your neovim configuration files
+		vim.keymap.set('n', '<leader>sn', function()
+			builtin.find_files { cwd = vim.fn.stdpath 'config' }
+		end, { desc = '[S]earch [N]eovim files' })
 	end
 }

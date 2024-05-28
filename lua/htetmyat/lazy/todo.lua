e3226d6cada829a9d69bafee30c291db0b256fd6
htetmyatthar <htetmyathm2002@gmail.com>
2024-05-28 06:32:16 +0630

    UPDATE: fomatted with lsp

--- lua/htetmyat/lazy/todo.lua
+++ lua/htetmyat/lazy/todo.lua
@@ -1,20 +1,20 @@
 return {
 	"folke/todo-comments.nvim",
-	dependencies = {"nvim-lua/plenary.nvim"},
+	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {
 		signs = false,
 		-- can customize here.
 		keywords = {
-			IDEA = { icon = " ", color = "default"},
-			ALERT = { icon = " ", color = "warning"},
-			DONE = { icon = " ", color = "green"},
-			todo = { icon = " ", color = "hint"}	-- small todo for my self.
+			IDEA = { icon = " ", color = "default" },
+			ALERT = { icon = " ", color = "warning" },
+			DONE = { icon = " ", color = "green" },
+			todo = { icon = " ", color = "hint" } -- small todo for myself.
 		},
 		colors = {
-			green = { "FIXED, FINISHED", "#31CB00"}
+			green = { "FIXED, FINISHED", "#31CB00" }
 		}
 	},
 
 	-- map to find the todos with telescope.
-	vim.api.nvim_set_keymap("n", "<leader>st", ":TodoTelescope<CR>", {noremap = true})
+	vim.api.nvim_set_keymap("n", "<leader>st", ":TodoTelescope<CR>", { noremap = true })
 }

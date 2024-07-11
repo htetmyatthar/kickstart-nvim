4137d1177429b8f8f2414d3ec40a3676ed76c34d
htetmyatthar <htetmyathm2002@gmail.com>
2024-07-11 21:10:08 +0630

    override the todo comment flags to remove all the icons

--- lua/htetmyat/lazy/todo.lua
+++ lua/htetmyat/lazy/todo.lua
@@ -5,10 +5,25 @@ return {
 		signs = false,
 		-- can customize here.
 		keywords = {
+			-- custom todo flags.
 			IDEA = { icon = " ", color = "default" },
 			ALERT = { icon = " ", color = "warning" },
 			DONE = { icon = " ", color = "green" },
-			todo = { icon = " ", color = "hint" } -- small todo for myself.
+			todo = { icon = " ", color = "hint" }, -- small todo for myself.
+
+			-- just to override the icon part of those flags.
+			FIX = {
+				icon = " ", -- icon used for the sign, and in search results
+				color = "error", -- can be a hex color, or a named color (see below)
+				alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+				-- signs = false, -- configure signs for some keywords individually
+			},
+			TODO = { icon = " ", color = "info" },
+			HACK = { icon = " ", color = "warning" },
+			WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+			PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+			NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+			TEST = { icon = " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
 		},
 		colors = {
 			green = { "FIXED, FINISHED", "#31CB00" }
